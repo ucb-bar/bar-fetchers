82206218dc7fc339acb7b86ff8c33262b85d9ece
Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>
2022-02-19 20:57:02 -0800

    Add timeliness

--- benchmarking/L1-benchmarking.py
+++ benchmarking/L1-benchmarking.py
@@ -1,8 +1,6 @@
 import sys
 
 def main():
-    #TODO: make parameterizable, write bash script to run configs automatically
-
     with open(sys.argv[1]) as f:
         prefetch_lines = f.readlines()
 
@@ -10,7 +8,7 @@ def main():
         no_prefetch_lines = f.readlines()
 
     misses_prevented = 0
-    prefetch_queue=[]
+    prefetch_queue={}
     prefetches_sent=[]
 
     no_prefetch = classify_accesses(no_prefetch_lines)
@@ -19,10 +17,6 @@ def main():
     with_prefetch = classify_accesses(prefetch_lines)
     with_prefetch_hits = with_prefetch['hits']
     with_prefetch_misses = with_prefetch['misses']
-    print(len(no_prefetch['hits']))
-    print(len(with_prefetch_hits))
-    print(len(no_prefetch_misses))
-    print(len(with_prefetch_misses))
 
     prefetch_hits_only = list(with_prefetch_hits)
     no_prefetch_misses_only = list(no_prefetch_misses)
@@ -33,50 +27,51 @@ def main():
     for addr in with_prefetch_misses:
         if addr in no_prefetch_misses_only:
             no_prefetch_misses_only.remove(addr)
-    #print(no_prefetch_misses)
-    #print(with_prefetch_misses)
-    #print(prefetch_hits_only)
-    #print(no_prefetch_misses_only)
 
-
-    # for miss in no_prefetch_misses['misses']:
-    #     print('miss: ' + miss)
-    # for hit in no_prefetch_misses['hits']:
-    #     print('hit: ' + hit)
-
-    useful_prefetches=[]
+    useful_prefetches=[] #prefetches that actually prevent a miss
+    num_prefetch_resps = 0
+    delta_sum = 0
+    num_prefetches_accessed = 0
 
     for line in prefetch_lines:
         if "Prefetch Addr" in line:
             pref = line.split()
-            #if len(prefetch_queue) == 1000:
-            #    prefetch_queue.pop() #remove least recently added addr
-            prefetches_sent.append(pref[4]) #add new prefetch address to queue
+            prefetches_sent.append(pref[4]) #add new prefetch address
         elif "Prefetch Resp" in line:
             pref_resp = line.split()
             pref_resp_addr = pref_resp[5]
+            pref_resp_cycles = int(pref_resp[1])
             if pref_resp_addr in prefetches_sent:
-                prefetch_queue.append(pref_resp_addr)
+                prefetch_queue[pref_resp_addr] = pref_resp_cycles #only interested in most recent response timing
+                num_prefetch_resps += 1
         elif "Snoop" in line:
             snoop = line.split()
             addr = snoop[4]
-            if ((addr in prefetch_queue) and (addr in no_prefetch_misses_only) and (addr in prefetch_hits_only)):
-                no_prefetch_misses_only.remove(addr) # make sure miss isn't counted twice
-                misses_prevented += 1
-                useful_prefetches.append(addr)
+            cycles = int(snoop[1])
+            if (addr in prefetch_queue):
+                delta_sum += (cycles - prefetch_queue[addr])
+                num_prefetches_accessed += 1
+                if ((addr in no_prefetch_misses_only) and (addr in prefetch_hits_only)):
+                    no_prefetch_misses_only.remove(addr) # make sure miss isn't counted twice
+                    prefetch_hits_only.remove(addr)
+                    misses_prevented += 1
+                    useful_prefetches.append(addr)
     
-    #Accuracy
-    num_no_resp_prefetches=len(prefetches_sent)-len(prefetch_queue)
-    num_unused_prefetches=len(prefetch_queue)-len(useful_prefetches)
+    #Accuracy Calculations
+    num_no_resp_prefetches=len(prefetches_sent)-num_prefetch_resps
+    num_unused_prefetches=num_prefetch_resps-len(useful_prefetches)
     useless_prefetches = num_no_resp_prefetches + num_unused_prefetches
-    print(num_no_resp_prefetches, num_unused_prefetches, useless_prefetches, len(useful_prefetches))
 
     print("misses prevented: " + str(misses_prevented))
+
     coverage = (misses_prevented + 0.0) / (misses_prevented + len(with_prefetch_misses)) * 100
     print("coverage: " + str(coverage) + "%")
 
     accuracy = (misses_prevented + 0.0) / (useless_prefetches + misses_prevented) * 100
     print("accuracy: " + str(accuracy) + "%")
+
+    timeliness = (delta_sum + 0.0) / num_prefetches_accessed
+    print("timeliness: " + str(timeliness) + " cycles")
 
 
 
@@ -104,7 +99,8 @@ def classify_accesses(lines):
                     accesses["hits"].append(resp_addr)
                 snoops.pop(resp_addr)
                 last_resp_cycle = resp[1]
-    #print(all_addr)
     return accesses
+
+
 
 main()
